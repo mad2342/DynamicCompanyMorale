ba4b3fdb021d3b7388f3c92686109e6d5af8535a
Mad <github@typ03.de>
2018-12-28 00:55:48 +0100

    update project info

--- Source/DynamicCompanyMorale/Properties/AssemblyInfo.cs
+++ Source/DynamicCompanyMorale/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("DynamicCompanyMorale")]
@@ -7,5 +6,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("DynamicCompanyMorale")]
 [assembly: ComVisible(false)]
 [assembly: Guid("9DD660E2-5851-46D5-AFE3-EA54782910EC")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.3.1.*")]
